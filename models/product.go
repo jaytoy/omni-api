53c535f361a0f186dcf355f7cf5cc095cb1a37a9
Jay Toy <jay_toyjianying@hotmail.com>
2022-10-13 10:12:29 +0200

    add product model

---
+++ models/product.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Product struct {
+	ID        uint      `json:"id"`
+	Name      string    `json:"product"`
+	Category  string    `json:"category"`
+	ImageURL  string    `json:"image"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
